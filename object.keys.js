52dc22210e5c842d437c94823b696eab057b9544
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-08-03 11:59:49 +0600

    some added

---
+++ object.keys.js
@@ -0,0 +1,21 @@
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+const key = Object.keys(myObject)
+console.log(key);
+console.log(typeof(myObject.name));
+console.log(typeof(myObject.age));
+console.log(typeof(myObject.city));
+console.log(typeof(myObject.isStudent));
+
+
+
+
+
+
+
--- student.js
+++ student.js
@@ -7,3 +7,30 @@ const student = {
         marks: 30
     }
 };
+console.log(student.physics.marks);
+student.physics['marks']= 50;
+console.log(student.physics['marks']);
+console.log(student);
+
+let student1 = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+console.log(student1.name);
+const boyos= student1.age
+console.log(boyos);
+console.log(student1.city);
+console.log(student1['isStudent']);
+student1.age = 25;
+console.log(student1.age);
+console.log(student1);
+
+
+
+
+
+
+
+
